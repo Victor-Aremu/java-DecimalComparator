1a3c4720355e46ca89df46e3a77e25624fc93e3a
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-16 17:10:26 +0000

    edited few lines

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,29 +1,22 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class DecimalComparator {
     public static void main(String[] args) {
 
-        boolean output = areEqualByThreeDecimalPlaces(-3.1756, -3.175);
-        System.out.println(output);
-
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
 
     }
     public static boolean areEqualByThreeDecimalPlaces (double decimal1, double decimal2){
 
+        decimal1 = (int) (decimal1 * 1000);
+        decimal2 = (int) (decimal2 * 1000);
 
-        BigDecimal dec1 = new BigDecimal(decimal1).setScale(3, RoundingMode.DOWN);
-        BigDecimal dec2 = new BigDecimal(decimal2).setScale(3, RoundingMode.DOWN);
-
-        System.out.println(dec1);
-        System.out.println(dec2);
-
-
-        if (dec1.equals(dec2)){
+        if (decimal1 == decimal2){
             return true;
         }
-
-        return false;
+        else {
+            return false;
+        }
     }
-
 }
