4a9e02a815039c67a47fa4d21257ef14e99d054d
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 16:28:02 +0000

    small changes made

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,17 +1,7 @@
 public class DecimalComparator {
-    public static void main(String[] args) {
-
-        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
-        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
-        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
-        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
-
-    }
     public static boolean areEqualByThreeDecimalPlaces (double decimal1, double decimal2){
-
         decimal1 = (int) (decimal1 * 1000);
         decimal2 = (int) (decimal2 * 1000);
-
         if (decimal1 == decimal2){
             return true;
         }
@@ -20,4 +10,4 @@ public class DecimalComparator {
         }
     }
 }
-//Victor-Aremu
+//Victor.Aremu
